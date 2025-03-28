a58b0a08725aa13efb9d7376ea973168a9e54220
Daniel Solá <daniel.sola@aizon.ai>
2025-03-28 12:00:01 +0100

    remove glue module

--- src/infra/modules/glue/script.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_s3_object" "glue_script" {
-    bucket = var.bucket_name
-    key = "glue/scripts/glue_script.py"
-    source = local.glue_src_path
-    etag = filemd5("${local.glue_src_path}")
-}
